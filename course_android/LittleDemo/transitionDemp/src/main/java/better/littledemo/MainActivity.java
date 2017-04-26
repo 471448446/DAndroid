699cbf41c1b86034cd82cccbf48925b2ba04fa52
yangchuan <yangchuan@yunzekeji.com>
2017-04-26 11:11:46 +0800

    feature transDemo

---
+++ course_android/LittleDemo/transitionDemp/src/main/java/better/littledemo/MainActivity.java
@@ -0,0 +1,32 @@
+package better.littledemo;
+
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView image, image2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        image = (ImageView) findViewById(R.id.imageTest);
+        image2 = (ImageView) findViewById(R.id.imageTest2);
+        TransitionDrawable transitionDrawable = new TransitionDrawable(new Drawable[]{new ColorDrawable(Color.parseColor("#303F9F")), new BitmapDrawable(getResources(), BitmapFactory.decodeResource(getResources(), R.drawable.smile))});
+        transitionDrawable.startTransition(1000);
+        TransitionDrawable transitionDrawable2 = (TransitionDrawable) ContextCompat.getDrawable(this, R.drawable.smile_drawable);
+        transitionDrawable2.startTransition(2000);
+
+        image.setImageDrawable(transitionDrawable);
+
+        image2.setImageDrawable(transitionDrawable2);
+    }
+}
