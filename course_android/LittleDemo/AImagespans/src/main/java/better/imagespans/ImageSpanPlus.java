030658a2a679101103c60e294a5db6f3ff859925
yangchuan <yangchuan@yunzekeji.com>
2017-06-28 14:32:42 +0800

    centerImageSpan

---
+++ course_android/LittleDemo/AImagespans/src/main/java/better/imagespans/ImageSpanPlus.java
@@ -0,0 +1,104 @@
+package better.imagespans;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.text.style.ImageSpan;
+import android.util.Log;
+
+/**
+ * Created by better on 2017/6/26 22:06.
+ */
+
+public class ImageSpanPlus extends ImageSpan {
+    /* 与文字中间对齐  --better 2017/6/27 16:55. */
+    public static final int ALIGN_CENTER = ALIGN_BASELINE + 1;
+    /* 与文字顶部对齐  --better 2017/6/27 16:56. */
+    public static final int ALIGN_TOP_ASCENT = ALIGN_CENTER + 1;
+
+    public ImageSpanPlus(Context context, @DrawableRes int resourceId, int verticalAlignment) {
+        super(context, resourceId, verticalAlignment);
+    }
+
+    public ImageSpanPlus(Context context, @DrawableRes int resourceId) {
+        super(context, resourceId);
+    }
+
+    /**
+     * 返回span的宽，同时设置Paint.FontMetricsInt的高度。这样就限定了当前span的大小
+     * Create By better on 2017/6/27 17:21.
+     */
+    public int getSize(Paint paint, CharSequence text, int start, int end, Paint.FontMetricsInt fm) {
+        log(text + "," + start + ",end" + end);
+        if (mVerticalAlignment == ALIGN_BASELINE || mVerticalAlignment == ALIGN_BOTTOM) {
+            return super.getSize(paint, text, start, end, fm);
+        }
+
+        Drawable d = getDrawable();
+        Rect rect = d.getBounds();
+        if (fm != null) {
+            Paint.FontMetricsInt fmPaint = paint.getFontMetricsInt();
+            int fontHeight = fmPaint.bottom - fmPaint.top;
+            int drHeight = rect.bottom - rect.top;
+
+            int top = drHeight / 2 - fontHeight / 4;
+            int bottom = drHeight / 2 + fontHeight / 4;
+
+            fm.ascent = -bottom;
+            fm.top = -bottom;
+            fm.bottom = top;
+            fm.descent = top;
+        }
+        return rect.right;
+    }
+
+    /**
+     * 确定span的位置
+     * Create By better on 2017/6/27 17:22.
+     */
+    @Override
+    public void draw(Canvas canvas, CharSequence text, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+        if (mVerticalAlignment == ALIGN_BASELINE ||
+                mVerticalAlignment == ALIGN_BOTTOM) {
+            super.draw(canvas, text, start, end, x, top, y, bottom, paint);
+            return;
+        }
+        if (mVerticalAlignment == ALIGN_CENTER) {
+            alignCenter(canvas, start, end, x, top, y, bottom, paint);
+            return;
+        }
+        if (mVerticalAlignment == ALIGN_TOP_ASCENT) {
+            alignTop(canvas, start, end, x, top, y, bottom, paint);
+            return;
+        }
+    }
+
+    private void alignTop(Canvas canvas, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+        Drawable drawable = getDrawable();
+        Paint.FontMetricsInt metricsInt = paint.getFontMetricsInt();
+        int transY = y + metricsInt.ascent;
+
+        canvas.save();
+        canvas.translate(x, transY);
+        drawable.draw(canvas);
+        canvas.restore();
+    }
+
+    private void alignCenter(Canvas canvas, int start, int end, float x, int top, int y, int bottom, Paint paint) {
+        Drawable drawable = getDrawable();
+        Paint.FontMetricsInt metricsInt = paint.getFontMetricsInt();
+        int transY = (y + metricsInt.descent + y + metricsInt.ascent) / 2 - drawable.getBounds().bottom / 2;
+
+        canvas.save();
+        canvas.translate(x, transY);
+        drawable.draw(canvas);
+        canvas.restore();
+    }
+
+    private void log(String msg) {
+        Log.d("ImageSpanPlus", msg);
+    }
+}
---
+++ course_android/LittleDemo/AImagespans/src/main/java/better/imagespans/MainActivity.java
@@ -0,0 +1,81 @@
+package better.imagespans;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.style.ImageSpan;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    String left = "文理学院";
+    String img = "图";
+    String right = "春熙路";
+    String leftLong = "文理学院院到当前为当前无无群无群多多群无多群无群";
+    String rightLong = "春熙路院到当前为当前无无群无群多多群无多群无群";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        normal();
+        vertical();
+        top();
+    }
+
+    private void top() {
+        TextView normal = (TextView) findViewById(R.id.span_top);
+        TextView normal2 = (TextView) findViewById(R.id.span_top2);
+
+        SpannableStringBuilder stringBuilder = getStrLong();
+        stringBuilder.setSpan(new ImageSpanPlus(this, R.drawable.goback, ImageSpanPlus.ALIGN_TOP_ASCENT), leftLong.length(), stringBuilder.length() - rightLong.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder stringBuilder2 = getStr();
+        stringBuilder2.setSpan(new ImageSpanPlus(this, R.drawable.go, ImageSpanPlus.ALIGN_TOP_ASCENT), left.length(), stringBuilder2.length() - right.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        normal.setText(stringBuilder);
+        normal2.setText(stringBuilder2);
+    }
+
+    private void vertical() {
+        TextView normal = (TextView) findViewById(R.id.span_vertical);
+        TextView normal2 = (TextView) findViewById(R.id.span_vertical2);
+        TextView normal3 = (TextView) findViewById(R.id.span_vertical3);
+
+        SpannableStringBuilder stringBuilder = getStrLong();
+        stringBuilder.setSpan(new ImageSpanPlus(this, R.drawable.goback, ImageSpanPlus.ALIGN_CENTER), leftLong.length(), stringBuilder.length() - rightLong.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder stringBuilder2 = getStr();
+        stringBuilder2.setSpan(new ImageSpanPlus(this, R.drawable.go, ImageSpanPlus.ALIGN_CENTER), left.length(), stringBuilder2.length() - right.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder stringBuilder3 = getStrLong();
+        stringBuilder3.setSpan(new ImageSpanPlus(this, R.mipmap.ic_launcher, ImageSpanPlus.ALIGN_CENTER), leftLong.length(), stringBuilder3.length() - rightLong.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        normal.setText(stringBuilder);
+        normal2.setText(stringBuilder2);
+        normal3.setText(stringBuilder3);
+    }
+
+    private void normal() {
+        TextView normal = (TextView) findViewById(R.id.span_normal);
+        TextView normal2 = (TextView) findViewById(R.id.span_normal2);
+
+        SpannableStringBuilder stringBuilder = getStr();
+        stringBuilder.setSpan(new ImageSpan(this, R.drawable.goback, ImageSpan.ALIGN_BOTTOM), left.length(), stringBuilder.length() - right.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        SpannableStringBuilder stringBuilder2 = getStr();
+        stringBuilder2.setSpan(new ImageSpan(this, R.drawable.go, ImageSpan.ALIGN_BOTTOM), left.length(), stringBuilder.length() - right.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        normal.setText(stringBuilder);
+        normal2.setText(stringBuilder2);
+    }
+
+    private SpannableStringBuilder getStr() {
+        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(left);
+        stringBuilder.append(img);
+        stringBuilder.append(right);
+        return stringBuilder;
+    }
+
+    private SpannableStringBuilder getStrLong() {
+        SpannableStringBuilder stringBuilder = new SpannableStringBuilder(leftLong);
+        stringBuilder.append(img);
+        stringBuilder.append(rightLong);
+        return stringBuilder;
+    }
+}
